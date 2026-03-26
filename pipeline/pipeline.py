82f7bb2336c73098e3f9b138a9f990f3dda88fb6
CHKSmith25 <chs.smith89@gmail.com>
2026-03-26 15:13:15 +0000

    initial commit

---
+++ pipeline/pipeline.py
@@ -0,0 +1,4 @@
+"""Full ETL pipeline for the project. 
+    1. Extracts data from websites
+    2. Transforms data into a clean format and calls the LLM to generate insights
+    3. Loads the insights into a RDS database"""
